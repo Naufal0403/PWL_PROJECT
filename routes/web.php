da1e64d6214a9e7c8521c9cd712bc9ee979e7125
= <=>
2021-06-03 08:16:12 +0700

    Membuat Routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KabupatenController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('logout', [Auth::class, 'logout'], function () {
+    return abort(404);
+});
+
+Route::resource('kabupaten', KabupatenController::class)->middleware('auth');
+Route::prefix('user')->middleware('auth')->group(function(){
+    Route::get('/kabupaten', [KabupatenController::class, 'kabupaten'])->name('user.kabupaten');
 });
